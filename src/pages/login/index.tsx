bea48939c83b9381d1b10ddf57e4dc43030fd433
lxnda <lindxlopez@gmail.com>
2023-06-06 00:40:26 +0200

    ,

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,3 +1,8 @@
+import React, { useState } from "react";
+import { LoginValidate } from "../../utils/validateForms";
+import axios from "axios";
+import { NavLink, useNavigate } from "react-router-dom";
+import { useNotification } from "../../context/notification.context";
 import {
   Box,
   Button,
@@ -7,16 +12,12 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
-import React from "react";
-import { LoginValidate } from "../../utils/validateForms";
-import axios from "axios";
-import { NavLink, useNavigate } from "react-router-dom";
-import { useNotification } from "../../context/notification.context";
 
 type LoginType = {
   email: string;
   contrasena: string;
 };
+
 type Idtype = {
   idEmp: number;
 };
@@ -30,12 +31,15 @@ const URLApivalidate = "http://127.0.0.1:6001/verificarUser";
 
 export const LoginPage: React.FC<{}> = () => {
   const { getError, getSuccess } = useNotification();
-  const [loginData, setLoginData] = React.useState<LoginType>({
+  const [loginData, setLoginData] = useState<LoginType>({
     email: "",
     contrasena: "",
   });
+  const [errors, setErrors] = useState<Validatetype>({
+    validate: false,
+  });
 
-  const handlechange = (e: { target: { name: any; value: any } }) => {
+  const handlechange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setLoginData((prevState) => ({
       ...prevState,
@@ -45,31 +49,30 @@ export const LoginPage: React.FC<{}> = () => {
   };
 
   //obtener idempresa
-  const [idEmp, setIdEmp] = React.useState<Idtype>({
+  const [idEmp, setIdEmp] = useState<Idtype>({
     idEmp: 0,
   });
 
   const peticionesPost = async () => {
-    await axios
-      .post(URLApi, { email: loginData.email })
-      .then((response) => {
-        setIdEmp(response.data.id);
-        if (response.data.id != 0) {
-          localStorage.setItem("idEmpresa", response.data.id);
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    try {
+      const response = await axios.post(URLApi, { email: loginData.email });
+      setIdEmp(response.data.id);
+      if (response.data.id !== 0) {
+        localStorage.setItem("idEmpresa", response.data.id);
+      }
+    } catch (error) {
+      console.error(error);
+    }
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLInputElement>) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     LoginValidate.validate(loginData)
       .then(() => {
         peticionesPost();
       })
       .catch((error) => {
+        setErrors({ validate: true });
         getError(error.message);
       });
   };
@@ -78,20 +81,18 @@ export const LoginPage: React.FC<{}> = () => {
   const navigate = useNavigate();
 
   const verificarPost = async () => {
-    await axios
-      .post(URLApivalidate, loginData)
-      .then((response) => {
-        if (response.data) {
-          navigate("/clientes");
-          getSuccess(JSON.stringify("validado"));
-        } else {
-          navigate("/");
-          getError("Usuario no Validado");
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    try {
+      const response = await axios.post(URLApivalidate, loginData);
+      if (response.data) {
+        navigate("/home");
+        getSuccess(JSON.stringify("validado"));
+      } else {
+        navigate("/");
+        getError("Usuario no Validado");
+      }
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
@@ -103,9 +104,21 @@ export const LoginPage: React.FC<{}> = () => {
       sx={{ minHeight: "100vh" }}
     >
       <Grid item>
-        <Paper sx={{ padding: "1.2em", borderRadius: "0.5em" }}>
+        <Paper
+          sx={{
+            padding: "1.2em",
+            borderRadius: "0.5em",
+            border: "2px solid #FDFFC1",
+            boxShadow: "0 0 8px #FDFFC1",
+            transition: "border-color 0.3s, box-shadow 0.3s",
+            "&:hover": {
+              borderColor: "#C6FFC1",
+              boxShadow: "0 0 15px #C6FFC1",
+            },
+          }}
+        >
           <Typography sx={{ mt: 1, mb: 1 }} variant="h4">
-            Iniciar sesion
+            Iniciar sesión
           </Typography>
           <Box component="form" onSubmit={handleSubmit}>
             <TextField
@@ -114,38 +127,55 @@ export const LoginPage: React.FC<{}> = () => {
               fullWidth
               label="Email"
               type="text"
-              sx={{ mt: 2, mb: 1.5 }}
+              sx={{
+                mt: 1.5,
+                mb: 1.5,
+                "&:hover": {
+                  borderColor: "#C6FFC1",
+                  boxShadow: "0 0 8px #C6FFC1",
+                },
+              }}
               onChange={handlechange}
+              error={errors.validate}
             />
             <TextField
               name="contrasena"
               margin="normal"
               fullWidth
               type="password"
-              label="Password"
-              sx={{ mt: 1.5, mb: 1.5 }}
+              label="Contraseña"
+              sx={{
+                mt: 1.5,
+                mb: 1.5,
+                "&:hover": {
+                  borderColor: "#C6FFC1",
+                  boxShadow: "0 0 8px #C6FFC1",
+                },
+              }}
               onChange={handlechange}
+              error={errors.validate}
             />
             <Button
               fullWidth
               variant="outlined"
               type="submit"
-              sx={{ mt: 1.5, mb: 3 }}
+              sx={{
+                mt: 1.5,
+                mb: 3,
+              }}
               onClick={() => verificarPost()}
             >
-              Iniciar sesion
+              Iniciar sesión
             </Button>
             <Grid container>
               <Grid item xs>
                 <Link href="#" variant="body2">
-                  Forgot password?
+                  ¿Olvidaste tu contraseña?
                 </Link>
               </Grid>
               <Grid item>
                 <NavLink to="/register">
-                  <Link variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
+                  <Link variant="body2">¿No tienes una cuenta? Regístrate</Link>
                 </NavLink>
               </Grid>
             </Grid>
--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,74 +1,152 @@
+import React, { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import axios from "axios";
 import {
   Box,
   Button,
   Checkbox,
   Container,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
   FormControlLabel,
-  FormGroup,
   Grid,
   Link,
   Paper,
-  Stack,
   TextField,
   Typography,
 } from "@mui/material";
-import React from "react";
 import { useNotification } from "../../context/notification.context";
-import { LoginValidate } from "../../utils/validateForms";
-import { NavLink, useNavigate } from "react-router-dom";
-import axios from "axios";
 
-type LoginType = {
+type RegisterType = {
   email: string;
   nombre: string;
   direccion: string;
-  telefono: number;
+  telefono: string;
   contrasena: string;
 };
 
 const URLApi = "http://127.0.0.1:6001/addUser";
 
 export const RegisterPage: React.FC<{}> = () => {
+  const navigate = useNavigate();
   const { getError, getSuccess } = useNotification();
-  const [registerData, setRegisterData] = React.useState<LoginType>({
+  const [registerData, setRegisterData] = useState<RegisterType>({
     email: "",
     nombre: "",
     direccion: "",
-    telefono: 0,
+    telefono: "",
     contrasena: "",
   });
+  const [termsModalOpen, setTermsModalOpen] = useState(false);
+  const [termsAccepted, setTermsAccepted] = useState(false);
 
-  const handlechange=(e: { target: { name: any; value: any; }; })=>{
-    const {name,value}=e.target;
-    setRegisterData(prevState=>({
+  const handlechange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setRegisterData((prevState) => ({
       ...prevState,
-      [name]:value
+      [name]: value,
     }));
-    console.log(registerData);
-  }
+  };
 
-  const handleSubmit = (e: React.FormEvent<HTMLInputElement>) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    LoginValidate.validate(registerData)
-      .then(() => {
-        getSuccess(JSON.stringify("Usuario validado"));
-      })
-      .catch((error) => {
-        getError(error.message);
-      });
+
+    if (!termsAccepted) {
+      getError("Debe aceptar los términos de privacidad");
+      return;
+    }
+
+    const validationErrors = validateForm();
+
+    if (Object.keys(validationErrors).length === 0) {
+      getSuccess("Usuario validado");
+      peticionesPost()
+        .then(() => {
+          getSuccess("¡Yay! Usuario creado correctamente!");
+          setTimeout(() => {
+            navigate("/");
+          }, 2000);
+        })
+        .catch((error) => {
+          getError(error.message);
+        });
+    } else {
+      const errorMessages = Object.entries(validationErrors).map(
+        ([field, message]) => `${field}: ${message}`
+      );
+      getError(errorMessages.join("\n"));
+    }
   };
 
-  //enviar los datos 
+  const validateForm = () => {
+    const validationErrors: Partial<RegisterType> = {};
+
+    // Expresión regular para validar el email
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    // Expresión regular para validar el teléfono (solo números)
+    const phoneRegex = /^\d+$/;
+
+    // Expresión regular para validar el nombre de empresa
+    const nombreRegex = /^[a-zA-Z0-9\s\-]+$/;
+
+    // Validación para el campo 'email'
+    if (!registerData.email) {
+      validationErrors.email = "Campo requerido";
+    } else if (!emailRegex.test(registerData.email)) {
+      validationErrors.email = "Email inválido";
+    }
+
+    // Validación para el campo 'nombre'
+    if (!registerData.nombre) {
+      validationErrors.nombre = "Campo requerido";
+    } else if (!nombreRegex.test(registerData.nombre)) {
+      validationErrors.nombre = "Nombre de empresa inválido";
+    }
+
+    // Validación para el campo 'direccion'
+    if (!registerData.direccion) {
+      validationErrors.direccion = "Campo requerido";
+    }
+
+    // Validación para el campo 'telefono'
+    if (!registerData.telefono) {
+      validationErrors.telefono = "Campo requerido";
+    } else if (!phoneRegex.test(registerData.telefono)) {
+      validationErrors.telefono = "Teléfono inválido";
+    }
+
+    // Validación para el campo 'contrasena'
+    if (!registerData.contrasena) {
+      validationErrors.contrasena = "Campo requerido";
+    }
+
+    return validationErrors;
+  };
+
   const peticionesPost = async () => {
-    await axios.post(URLApi, registerData)
-      .then(response=>(
-        console.log(response)
-      ))
-      .catch((error) => {
-        console.error(error);
-      });
+    try {
+      const response = await axios.post(URLApi, registerData);
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
+  const handleTermsModalOpen = () => {
+    setTermsModalOpen(true);
+  };
+
+  const handleTermsModalClose = () => {
+    setTermsModalOpen(false);
+  };
+
+  const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTermsAccepted(e.target.checked);
+  };
 
   return (
     <Container maxWidth="sm">
@@ -80,9 +158,13 @@ export const RegisterPage: React.FC<{}> = () => {
         sx={{ minHeight: "100vh" }}
       >
         <Grid item>
-          <Paper sx={{ padding: "1.2em", borderRadius: "0.5em" }}>
+          <Paper  sx={{
+              padding: "1.2em",
+              borderRadius: "0.5em",
+              border: "2px solid #C6FFC1",
+              boxShadow: "0 0 8px #C6FFC1",
+            }}>
             <Typography sx={{ mt: 1, mb: 1 }} variant="h4">
-              
               Crear Cuenta
             </Typography>
             <Box component="form" onSubmit={handleSubmit}>
@@ -95,73 +177,150 @@ export const RegisterPage: React.FC<{}> = () => {
                 sx={{ mt: 2, mb: 1.5 }}
                 onChange={handlechange}
               />
-              <Grid container direction="row" justifyContent="space-beetwen">
-                <Grid item>
+              <Grid
+                container
+                direction="row"
+                justifyContent="space-between"
+                sx={{ mb: 1.5 }}
+              >
+                <Grid item xs={6}>
                   <TextField
                     name="nombre"
                     margin="normal"
                     fullWidth
                     label="Nombre Empresa"
                     type="text"
-                    sx={{ mt: 2, mb: 1.5 }}
+                    sx={{ mt: 2 }}
                     onChange={handlechange}
                   />
                 </Grid>
-                <Grid item>
+                <Grid item xs={6}>
                   <TextField
                     name="direccion"
                     margin="normal"
                     fullWidth
-                    label="Direccion"
+                    label="Dirección"
                     type="text"
-                    sx={{ mt: 2, mb: 1.5 }}
+                    sx={{ mt: 2 }}
                     onChange={handlechange}
                   />
                 </Grid>
-                
               </Grid>
-              <Grid item>
-                  <TextField
-                    name="telefono"
-                    margin="normal"
-                    fullWidth
-                    label="Telefono"
-                    type="phone"
-                    sx={{ mt: 2, mb: 1.5 }}
-                    onChange={handlechange}
-                  />
-                </Grid>
+              <TextField
+                name="telefono"
+                margin="normal"
+                fullWidth
+                label="Teléfono"
+                type="text"
+                sx={{ mt: 1.5, mb: 1.5 }}
+                onChange={handlechange}
+              />
               <TextField
                 name="contrasena"
                 margin="normal"
                 fullWidth
                 type="password"
-                label="Constraseña"
+                label="Contraseña"
                 sx={{ mt: 1.5, mb: 1.5 }}
                 onChange={handlechange}
               />
-              <FormGroup>
-                <FormControlLabel
-                  control={<Checkbox defaultChecked />}
-                  label="Aceptas los terminos de privacidad etc.."
-                />
-              </FormGroup>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={termsAccepted}
+                    onChange={handleCheckboxChange}
+                  />
+                }
+                label={
+                  <Typography variant="body2">
+                    Aceptas los términos de privacidad{" "}
+                    <Link
+                      component="button"
+                      variant="body2"
+                      onClick={handleTermsModalOpen}
+                    >
+                      (Ver términos)
+                    </Link>
+                  </Typography>
+                }
+              />
               <Button
                 fullWidth
                 variant="outlined"
                 type="submit"
                 sx={{ mt: 1.5, mb: 3 }}
-                onClick={()=> peticionesPost()}
               >
                 Crear cuenta
               </Button>
               <NavLink to="/">
-                <Link variant="body2">{"have an account? Sign In"}</Link>
+                <Link variant="body2">
+                  {"¿Ya tienes una cuenta? Inicia sesión"}
+                </Link>
               </NavLink>
             </Box>
           </Paper>
         </Grid>
       </Grid>
+      <Dialog
+        open={termsModalOpen}
+        onClose={handleTermsModalClose}
+        PaperProps={{
+          sx: {
+            backgroundColor: "#1D211D",
+            border: "2px solid #C6FFC1",
+            boxShadow: "0 0 8px #C6FFC1",
+            opacity: 0.9,
+            color: "white",
+          },
+        }}
+      >
+        <DialogTitle>Términos de privacidad</DialogTitle>
+        <DialogContent>
+          <DialogContentText sx={{ color: "white" }}>
+            Nuestra prioridad es proteger tu información personal. A
+            continuación, se presentan los puntos clave de nuestra política de
+            privacidad: <br />
+            <br />
+            <b>Recopilación de información:</b> Podemos recopilar información
+            personal que nos proporcionas al utilizar nuestros servicios. <br />
+            <br />
+            <b>Uso de la información:</b> Utilizamos tu información para
+            brindarte nuestros servicios y mejorar tu experiencia como usuario.{" "}
+            <br />
+            <br />
+            <b>Protección de la información:</b> Implementamos medidas de
+            seguridad para proteger tus datos personales y prevenir su acceso no
+            autorizado. <br />
+            <br />
+            <b>Compartir información:</b> No compartimos tu información personal
+            con terceros, excepto cuando sea necesario para brindarte nuestros
+            servicios o cumplir con la ley. <br />
+            <br />
+            <b>Tus derechos:</b> Tienes derecho a acceder, corregir y eliminar
+            tus datos personales. Puedes contactarnos para ejercer estos
+            derechos. <br />
+            <br />
+            <b>Cambios en la política:</b> Podemos actualizar nuestra política
+            de privacidad. <br />
+            <br />
+            Te notificaremos sobre cualquier cambio relevante. <br />
+            Si tienes alguna pregunta o inquietud sobre nuestra política de
+            privacidad, no dudes en contactarnos.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={handleTermsModalClose}
+            sx={{
+              color: "white",
+              backgroundColor: "#798B78",
+              border: "2px solid #798B78",
+            }}
+          >
+            Cerrar
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Container>
   );
 };
